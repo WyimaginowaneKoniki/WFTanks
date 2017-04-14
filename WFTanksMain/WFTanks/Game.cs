6b1e09b2ccc71a4eafc028bd44973122a8816fce
D4Tne <sonny98@interia.pl>
2017-04-14 16:13:05 +0200

    Add files

---
+++ WFTanksMain/WFTanks/Game.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFTanks
+{
+    class Game
+    {
+        public enum GameStates
+        {
+            Win,
+            Lose,
+            Draw,
+            InGame
+        }
+        public GameStates CurrentGameState;
+        private uint _score;
+
+        public uint Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+
+
+        public bool Collisions()
+        {
+            return true;
+        }
+
+
+    }
+}
---
+++ WFTanksMain/WFTanks/Tanks.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFTanks
+{
+    abstract class Tanks
+    {
+        protected enum Levels
+        {
+            Small,
+            Medium,
+            Big
+        }
+        protected enum Sides
+        {
+            AI,
+            Buddy
+        }
+        protected Sides Side;
+
+        private uint _currentPositionX;
+
+        public uint CurrentPositionX
+        {
+            get { return _currentPositionX; }
+            set { _currentPositionX = value; }
+        }
+
+        private uint _currentPositionY;
+
+        public uint CurrentPositionY
+        {
+            get { return _currentPositionY; }
+            set { _currentPositionY = value; }
+        }
+
+        private int _healthPoints;
+
+        public int HealthPoints
+        {
+            get { return _healthPoints; }
+            set { _healthPoints = value; }
+        }
+
+        private string _playerName;
+
+        public string PlayerName
+        {
+            get { return _playerName; }
+            set { _playerName = value; }
+        }
+
+
+        protected Levels CurrentLevel;
+        // TODO Skin!!!!!
+        public virtual void Shot()
+        {
+
+        }
+
+        public virtual void Movement()
+        {
+
+        }
+    }
+}
