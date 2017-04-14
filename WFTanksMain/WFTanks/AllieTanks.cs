f0006c399290fd1aaa78f2381bf642b04d09023f
D4Tne <sonny98@interia.pl>
2017-04-14 16:22:59 +0200

    Update files

---
+++ WFTanksMain/WFTanks/AllieTanks.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFTanks
+{
+    class AllieTanks : Tanks
+    {
+
+        public override void Shot()
+        {
+
+        }
+
+        public override void Movement()
+        {
+
+        }
+    }
+}
--- WFTanksMain/WFTanks/Form1.Designer.cs
+++ WFTanksMain/WFTanks/Form1.Designer.cs
@@ -28,12 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AllieTanksDesign = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.AllieTanksDesign)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AllieTanksDesign
+            // 
+            this.AllieTanksDesign.BackColor = System.Drawing.Color.Transparent;
+            this.AllieTanksDesign.Image = ((System.Drawing.Image)(resources.GetObject("AllieTanksDesign.Image")));
+            this.AllieTanksDesign.Location = new System.Drawing.Point(173, 114);
+            this.AllieTanksDesign.Name = "AllieTanksDesign";
+            this.AllieTanksDesign.Size = new System.Drawing.Size(30, 30);
+            this.AllieTanksDesign.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.AllieTanksDesign.TabIndex = 0;
+            this.AllieTanksDesign.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.AllieTanksDesign);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);        
+            ((System.ComponentModel.ISupportInitialize)(AllieTanksDesign)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox AllieTanksDesign;
     }
 }
 
